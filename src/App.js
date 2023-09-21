957cebc7d5d04509fd1d372e11a26e3fa2da4ca4
Araz Kubtanjian <arazkubtanjian@gmail.com>
2023-09-20 22:48:39 -0400

    added more text to test push

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
+class App() ;  
+  
+export default App;
+extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
      <p>Hello, World!</p>
      <p>I am learning how to use Linux!</p>
+     <h2>My name is Araz.</h2>
+     <p>Now making additional changes to push to GitHub</p>
       </div>
-      </div>
-    );
+  </div> 
+   );
 }
 }
-export default App;
