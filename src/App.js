4d0db2a1eb24e83fa6c096e98b920ddbc04d005a
Araz Kubtanjian <arazkubtanjian@gmail.com>
2023-09-20 23:09:00 -0400

    Changes made to the whole text of App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App() ;  
-  
-export default App;
-extends Component {
+
+class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-     <p>Hello, World!</p>
-     <p>I am learning how to use Linux!</p>
-     <h2>My name is Araz.</h2>
-     <p>Now making additional changes to push to GitHub</p>
-      </div>
-  </div> 
-   );
+          <h1>Changes have been made to my file!</h1>
+        </div>
+      </div> 
+    );
+  }
 }
-}
+
+export default App;
